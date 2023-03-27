31a0c15a173c2ad6267c7412fa047cc9dbc43b4c
Aaron Alvarez <aaron.alvarez@unosquare.com>
2023-03-27 13:33:41 -0600

    implementado gherkin con funcionalidad POM anterior

--- cypress/stepDefinitions/iphonePriceSteps.js
+++ cypress/stepDefinitions/iphonePriceSteps.js
@@ -1,14 +1,24 @@
 /// <reference types="Cypress" />
+
+import {mainPage} from "../support/PageObject/Pages/MainPage.js";
+import {resultPage} from "../support/PageObject/Pages/ResultPage.js"
+import {itemPage} from "../support/PageObject/Pages/ItemPage.js";
+const items = require('../support/PageObject/testData/Items.json');
+const locators = require('../support/PageObject/testData/locators.json');
+const validators = require('../support/PageObject/testData/validationText.json');
+
 import {Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
 
-Given('I navigate to automation exercise website', () => {
-cy.visit('https://automationexercise.com/');
+Given('I navigate to amazon.com', () => {
+cy.visit('www.amazon.com/');
 })
 
-When('I enter login credentials', () => {
-
+When('I type Iphone 14 Pro Max in Searchbar I click the Search Button I click on the first search result', () => {
+mainPage.searchProduct(items.Iphone14ProMax)
+mainPage.clickSearch();
+resultPage.clickFirstResult();
 })
 
-Then('I should be logged in', () => {
-
+Then('Assert that the price is not empty assert that the price is not null assert that the price is greater than 0 Log the price in the console', () => {
+itemPage.logItemPrice();
 })
